e8cddd76cfd348023fa3de768f741cd8d77fccdd
ssuverkropp <S.Suverkropp@student.science.ru.nl>
2024-12-02 17:31:43 +0100

    solve day 2 part 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,9 @@
 module Main (main) where
 
-import Day02 (part1, handleInput)
+import Day02 (part2, handleInput)
 import Utils (runDay)
 
 main :: IO ()
 main = do
-  output <- runDay 2 (part1 . handleInput)
+  output <- runDay 2 (part2 . handleInput)
   putStrLn output
--- src/Day02.hs
+++ src/Day02.hs
@@ -1,6 +1,7 @@
 module Day02
   ( part1,
-    handleInput
+    part2,
+    handleInput,
   )
 where
 
@@ -12,24 +13,34 @@ handleInput = map (map read . words) . lines
 part1 :: [[Int]] -> Int
 part1 = count isSafe
 
+part2 :: [[Int]] -> Int
+part2 = count (any isSafe . dampen)
+
 isSafe :: [Int] -> Bool
 isSafe xs = case xs of
-    [] -> True
-    [_] -> True
-    (x:y:_) | x > y -> isSafeDecreasing xs
-    (x:y:_) | x < y -> isSafeIncreasing xs
-    _ -> False
+  [] -> True
+  [_] -> True
+  (x : y : _) | x > y -> isSafeDecreasing xs
+  (x : y : _) | x < y -> isSafeIncreasing xs
+  _ -> False
 
 isSafeDecreasing :: [Int] -> Bool
 isSafeDecreasing [] = True
 isSafeDecreasing [_] = True
-isSafeDecreasing (x:y:xs)
-    | x>y && x-y<4 = isSafeDecreasing (y:xs)
-    | otherwise = False
+isSafeDecreasing (x : y : xs)
+  | x > y && x - y < 4 = isSafeDecreasing (y : xs)
+  | otherwise = False
 
 isSafeIncreasing :: [Int] -> Bool
 isSafeIncreasing [] = True
 isSafeIncreasing [_] = True
-isSafeIncreasing (x:y:xs)
-    | x<y && y-x<4 = isSafeIncreasing (y:xs)
-    | otherwise = False
+isSafeIncreasing (x : y : xs)
+  | x < y && y - x < 4 = isSafeIncreasing (y : xs)
+  | otherwise = False
+
+dampen :: [Int] -> [[Int]]
+dampen = helper []
+  where
+    helper :: [Int] -> [Int] -> [[Int]]
+    helper _ [] = []
+    helper xs (y : ys) = (xs ++ ys) : helper (xs ++ [y]) ys
