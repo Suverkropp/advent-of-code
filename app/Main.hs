94210ebd1e0f975bc45d75e576df349884fc0c05
ssuverkropp <S.Suverkropp@student.science.ru.nl>
2024-12-07 12:47:42 +0100

    solve day 7

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,9 @@
 module Main (main) where
 
-import Day06 (part2, handleInput)
+import Day07 (part2, handleInput)
 import Utils (runDay)
 
 main :: IO ()
 main = do
-  output <- runDay 6 (part2 . handleInput)
+  output <- runDay 7 (part2 . handleInput)
   putStrLn output
